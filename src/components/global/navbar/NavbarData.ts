5036a250d38f429cef1c7d64c05ab4fcb6eeb06c
Saadienterprises1122 <saadienterprises1122@gmail.com>
2025-11-23 18:02:11 +0500

    Update NavbarData.ts

--- src/components/global/navbar/NavbarData.ts
+++ src/components/global/navbar/NavbarData.ts
@@ -2,7 +2,8 @@ import { NavItem } from './NavbarTypes';
 
 export const NAV_ITEMS: NavItem[] = [
   { label: 'Home', href: '/' },
-  { label: 'About', href: '/#about' },
-  { label: 'Work', href: '/#work' },
-  { label: 'Contact', href: '/#contact' },
+  { label: 'About', href: '/about' },
+  { label: 'Ai Lab', href: '/ai-lab' },
+  { label: 'Work', href: '/work' },
+  { label: 'Contact', href: '/contact' },
 ];
